df6f7ea75c17a88162fd1a899ffd7c268c6c4148
vinicius emanuel <vncemanuel@gmail.com>
2020-07-09 10:30:30 -0300

    base project 4

---
+++ Project_4/Project_4/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project_4
+//
+//  Created by Vinicius Emanuel on 08/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Project_4/Project_4/WindowController.swift
@@ -0,0 +1,18 @@
+//
+//  WindowController.swift
+//  Project_4
+//
+//  Created by Vinicius Emanuel on 09/07/20.
+//  Copyright © 2020 Personal. All rights reserved.
+//
+
+import Cocoa
+
+class WindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+        
+        window?.titleVisibility = .hidden
+    }
+}
