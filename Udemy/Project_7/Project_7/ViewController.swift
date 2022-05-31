675d9bc4b1635783ab653122e77e8fee75f5ca5f
vinicius emanuel <viniciusemanuel@viniciuss-MBP.Dlink>
2022-05-31 17:04:50 -0300

    drag and drop external to internal photos

--- Udemy/Project_7/Project_7/ViewController.swift
+++ Udemy/Project_7/Project_7/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: NSViewController, NSCollectionViewDelegate, NSCollectionVi
     @IBOutlet weak var collectionView: NSCollectionView!
     
     var photos = [URL]()
+    var itemsBeingDragged: Set<IndexPath>?
     
     lazy var photosDirectory: URL = {
         let fm = FileManager.default
         var paths = fm.urls(for: .documentDirectory, in: .userDomainMask)
         var saveDirectory = paths[0]
         saveDirectory.appendPathComponent("SlideMark")
+        print(saveDirectory.path)
         
         if fm.fileExists(atPath: saveDirectory.path) == false {
             try? fm.createDirectory(at: saveDirectory, withIntermediateDirectories: true)
@@ -31,6 +33,8 @@ class ViewController: NSViewController, NSCollectionViewDelegate, NSCollectionVi
 
         self.collectionView.delegate = self
         self.collectionView.dataSource = self
+        
+        self.collectionView.registerForDraggedTypes([.URL, .fileURL])
         
         self.photos = self.loadPhotos()
     }
@@ -54,7 +58,56 @@ class ViewController: NSViewController, NSCollectionViewDelegate, NSCollectionVi
         }
     }
     
+    func performInternalDrag(with items: [IndexPath], to indexPath: IndexPath) {
+        
+    }
+
+    func performExternalDrag(with items: [NSPasteboardItem], at indexPath: IndexPath) {
+        let fm = FileManager.default
+        
+        for item in items {
+            guard let stringURL = item.string(forType: NSPasteboard.PasteboardType(kUTTypeFileURL as String)) else { continue }
+            guard let sourceURL = URL(string: stringURL) else { continue }
+            let destinationURL = photosDirectory.appendingPathComponent(sourceURL.lastPathComponent)
+
+            do {
+                try fm.copyItem(at: sourceURL, to: destinationURL)
+            } catch {
+                print(error.localizedDescription)
+            }
+
+            self.photos.insert(destinationURL, at: indexPath.item)
+            self.collectionView.insertItems(at: [indexPath])
+        }
+
+    }
+    
     //MARK: - NSCollectionViewDelegate
+    func collectionView(_ collectionView: NSCollectionView, draggingSession session: NSDraggingSession, willBeginAt screenPoint: NSPoint, forItemsAt indexPaths: Set<IndexPath>) {
+        self.itemsBeingDragged = indexPaths
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, draggingSession session: NSDraggingSession, endedAt screenPoint: NSPoint, dragOperation operation: NSDragOperation) {
+        self.itemsBeingDragged = nil
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, acceptDrop draggingInfo: NSDraggingInfo, indexPath: IndexPath, dropOperation: NSCollectionView.DropOperation) -> Bool {
+        if let moveItems = itemsBeingDragged?.sorted() {
+            //internal
+            self.performInternalDrag(with: moveItems, to: indexPath)
+        } else {
+            //external
+            let pasteboard = draggingInfo.draggingPasteboard
+            guard let items = pasteboard.pasteboardItems else { return true }
+            self.performExternalDrag(with: items, at: indexPath)
+        }
+        
+        return true
+    }
+    
+    func collectionView(_ collectionView: NSCollectionView, validateDrop draggingInfo: NSDraggingInfo, proposedIndexPath proposedDropIndexPath: AutoreleasingUnsafeMutablePointer<NSIndexPath>, dropOperation proposedDropOperation: UnsafeMutablePointer<NSCollectionView.DropOperation>) -> NSDragOperation {
+        return .move
+    }
     
     //MARK: - NSCollectionViewDataSource
     func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
