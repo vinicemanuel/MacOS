8bb2975479526048e1ef4ba2f410ac0d7f607560
vinicius emanuel <viniciusemanuel@viniciuss-MBP.Dlink>
2022-06-03 09:27:36 -0300

    hiden window and icon in dock

---
+++ Udemy/Project_10/Project_10/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project_10
+//
+//  Created by vinicius emanuel on 02/06/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+}
+
