2c04cb6d97a4d2eff647907f22488ff4cbee262b
vinicius emanuel <viniciusemanuel@viniciuss-MBP.Dlink>
2022-06-06 09:16:30 -0300

    base project 13

---
+++ Udemy/Project_13/Project_13/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Project_13
+//
+//  Created by vinicius emanuel on 06/06/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
